9574ad1b9c438a84f6f851bc2eeefa43bbf11ccb
Maxim Spiridonov <taijased@gmail.com>
2019-07-30 09:22:33 +0300

    Initial Commit

---
+++ CalculateCredit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculateCredit
+//
+//  Created by Maxim Spiridonov on 30/07/2019.
+//  Copyright © 2019 Maxim Spiridonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
